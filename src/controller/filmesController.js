1276344ff6f093f24b39cf07d09302cc91f7460c
letiiciia <leticialimaferreira2@gmail.com>
2021-03-26 13:05:01 -0300

    metodo patch

--- src/controller/filmesController.js
+++ src/controller/filmesController.js
@@ -1,21 +1,21 @@
 const filmes = require("../model/filmes.json");
 const helper = require('../helper/helper');
 
-const getAll = (request, response) =>{
-console.log(request.url);
-response.status(200).send(filmes);
+const getAll = (request, response) => {
+    console.log(request.url);
+    response.status(200).send(filmes);
 }
 
 const postFilmes = (request, response) => {
     console.log(request.url);
-    const {title, description} = request.body;
+    const { title, description } = request.body;
 
     const novofilme = {
-        
+
         "id": helper.obterNovoValor(filmes),
         "title": title,
         "description": description
-          
+
     }
 
     filmes.push(novofilme);
@@ -24,44 +24,43 @@ const postFilmes = (request, response) => {
 
 const putFilmes = (request, response) => {
     const filmeAtualizado = request.body;
-    const id = request.params.id;
+    const id = parseInt(request.params.id);
 
-    //  const filmeAtualizado = {
-        
-    //      "title": request.body,
-    //      "description": request.body
-          
-    //  }
+    const filmeIds = filmes.map(filmes => filmes.id)
 
-    for(var i = 0; i < filmes.length; i++){
-        if(filmes[i].id === id){
-            const index = filmes[i];
-            const filmeAtualizadoComId = {id, ...filmeAtualizado}
-            console.log(filmeAtualizadoComId);
-            filmes.splice(index, 1, filmeAtualizadoComId);
-            
-            return response.status(200).send("Atualizado com sucesso!");
-        }else{
-            return response.status(404).send("erro")
-        }
-       
-    }   
+    const atualizaId = filmeIds.indexOf(id)
+
+    const filmeAtualizadacomId = { id, ...filmeAtualizado }
+    filmes.splice(atualizaId, 1, filmeAtualizadacomId)
+
+    return response.status(200).send(filmes.find(filmes => filmes.id === id))
+
 }
 
+const patchFilmes = (request, response) => {
+    const filmeAtualizacao = request.body;
+    const id = parseInt(request.params.id);
+    const filmeParaAtualizar = filmes.find(filmes => filmes.id === id)
 
+    for (chave in filmeAtualizacao) {
+        filmeParaAtualizar[chave] = filmeAtualizacao[chave]
+    }
+
+    return response.status(200).send(filmeParaAtualizar);
+}
 
 const deleteFilmes = (request, response) => {
     const id = parseInt(request.params.id);
 
-    for(var i = 0; i < filmes.length; i++){
+    for (var i = 0; i < filmes.length; i++) {
         const index = filmes[i];
-        if(filmes[i].id === id){
-            filmes.splice(index,1);
+        if (filmes[i].id === id) {
+            filmes.splice(index, 1);
             return response.status(201).send("Apagado com sucesso!");
-        }else{
+        } else {
             return response.status(404).send("Não foi possível apagar o filme!")
         }
-    }   
+    }
 }
 
 
@@ -69,5 +68,6 @@ module.exports = {
     getAll,
     postFilmes,
     deleteFilmes,
-    putFilmes
+    putFilmes,
+    patchFilmes
 }
--- src/routes/filmesRoute.js
+++ src/routes/filmesRoute.js
@@ -5,6 +5,7 @@ const controller = require("../controller/filmesController");
 router.get("/", controller.getAll);
 router.post("/criar", controller.postFilmes);
 router.put("/atualiza/:id", controller.putFilmes);
+router.patch("/atualizaCampo/:id", controller.patchFilmes);
 router.delete("/:id", controller.deleteFilmes);
 
 
